db89b121a5ed6137f6bff9a1b210d96a09faf22a
Eunice <omgunis@gmail.com>
2016-10-11 19:11:41 -0700

    Add a few projects to test projects api

---
+++ src/api/projectsApi.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    id: "plastic-walrus",
+    title: "Plastic Walrus Inc",
+    type: "eCommerce, Website, Wordpress",
+    linkToSite: "http://plasticwalrus.com/",
+    description: "",
+    objectives: "Create an eCommerce site",
+    tech: "Wordpress, Google Analytics"
+  },
+  {
+    id: "rad-how-to-school",
+    title: "Rad How To School",
+    type: "Website, Wordpress",
+    linkToSite: "http://radhowtoschool.com/",
+    description: "",
+    objectives: "Create an engaging and modern site to provide visitors with information on the artist. Allow students to register for classes on the site.",
+    tech: "Wordpress"
+  },
+  {
+    id: "halfway-hang",
+    title: "Halfway Hang",
+    type: "App, Website",
+    linkToSite: "http://halfwayhang.herokuapp.com/map",
+    description: "Halfway Hang is app for users to find a middleplace to meet. Whether it be a coffee shop, restaurant, or bar, this app will help you find a meeting place with your friends!".
+    objectives: "User authentication, routing, design",
+    tech: "NodeJs, Express, Ajax, Mongo/Mongoose, HTML, CSS, Twitter Bootstrap, Facebook Authentication API, Google Maps API, Yelp API, Passport Authentication"
+  }
+];
--- src/components/project/ManageProjectPage.js
+++ src/components/project/ManageProjectPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+export class ManageProjectPage extends React.Component {
+  render(){
+    return(
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default ManageProjectPage;
