36a7ad642516eaefcd7337dd50375712d1a57f30
omgunis <eunicegchang@gmail.com>
2016-12-08 15:57:48 -0800

    Change color of navbar on scroll

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,27 +1,83 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
-import LoadingDots from './LoadingDots';
+import { bindActionCreators } from 'redux';
+{/* import LoadingDots from './LoadingDots'; */}
 
-const Header = ({ loading }) => {
-  return (
-    <nav className="sticky">
-      <IndexLink to="/" activeClassName="active" className="nav-item">HOME</IndexLink>
+class Header extends React.Component {
+  constructor(props, context){
+    super(props, context);
 
-      <Link to="/skills" activeClassName="active" className="nav-item">SKILLS</Link>
+    this.state = {
+      transform: -60
+    };
 
-      <Link to="/projects" activeClassName="active" className="nav-item">PROJECTS</Link>
+    this.handleScroll = this.handleScroll.bind(this);
+    {/* this.state = { loading }; */}
+  }
 
-      <Link to="/about" activeClassName="active" className="nav-item">ABOUT ME</Link>
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
-      <Link to="/contact" activeClassName="active" className="nav-item">CONTACT</Link>
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
-      { loading && <LoadingDots interval={ 100 } dots={ 20 }/> }
-    </nav>
-  );
-};
+  handleScroll(event) {
+    console.log("Scrolling!");
+    let scrollTop = event.srcElement.body.scrollTop, itemTranslate = Math.min(0, scrollTop/3 - 60);
 
-Header.propTypes = {
-  loading: PropTypes.bool.isRequired
-};
+    console.log("Scroll Top: ", scrollTop);
+    this.setState({
+      transform: itemTranslate
+    });
+    console.log("itemTranslate: ", itemTranslate);
+  }
+
+  render(){
+
+    let className = this.state.transform ? 'sticky' : 'scrolling sticky';
+
+    return (
+      <nav className={className}>
+        <IndexLink to="/" activeClassName="active" className="nav-item">HOME</IndexLink>
+
+        <Link to="/skills" activeClassName="active" className="nav-item">SKILLS</Link>
+
+        <Link to="/projects" activeClassName="active" className="nav-item">PROJECTS</Link>
+
+        <Link to="/about" activeClassName="active" className="nav-item">ABOUT ME</Link>
+
+        <Link to="/contact" activeClassName="active" className="nav-item">CONTACT</Link>
+
+        {/* { loading && <LoadingDots interval={ 100 } dots={ 20 }/> } */}
+      </nav>
+    );
+  }
+}
+{/*
+  const Header = ({ loading }) => {
+    return (
+      <nav className="sticky">
+        <IndexLink to="/" activeClassName="active" className="nav-item">HOME</IndexLink>
+
+        <Link to="/skills" activeClassName="active" className="nav-item">SKILLS</Link>
+
+        <Link to="/projects" activeClassName="active" className="nav-item">PROJECTS</Link>
+
+        <Link to="/about" activeClassName="active" className="nav-item">ABOUT ME</Link>
+
+        <Link to="/contact" activeClassName="active" className="nav-item">CONTACT</Link>
+
+        { loading && <LoadingDots interval={ 100 } dots={ 20 }/> }
+      </nav>
+    );
+  };
+*/}
+{/*
+  Header.propTypes = {
+    loading: PropTypes.bool.isRequired
+  };
+ */}
 
 export default Header;
