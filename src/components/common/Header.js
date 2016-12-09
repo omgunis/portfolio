c40981dd7ce76f45c1e252d4c9de60b545dd07ae
omgunis <eunicegchang@gmail.com>
2016-12-08 16:35:38 -0800

    install google-charts package

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import { bindActionCreators } from 'redux';
-{/* import LoadingDots from './LoadingDots'; */}
 
 class Header extends React.Component {
   constructor(props, context){
@@ -12,7 +11,6 @@ class Header extends React.Component {
     };
 
     this.handleScroll = this.handleScroll.bind(this);
-    {/* this.state = { loading }; */}
   }
 
   componentDidMount() {
@@ -24,14 +22,11 @@ class Header extends React.Component {
   }
 
   handleScroll(event) {
-    console.log("Scrolling!");
     let scrollTop = event.srcElement.body.scrollTop, itemTranslate = Math.min(0, scrollTop/3 - 60);
 
-    console.log("Scroll Top: ", scrollTop);
     this.setState({
       transform: itemTranslate
     });
-    console.log("itemTranslate: ", itemTranslate);
   }
 
   render(){
@@ -50,34 +45,9 @@ class Header extends React.Component {
 
         <Link to="/contact" activeClassName="active" className="nav-item">CONTACT</Link>
 
-        {/* { loading && <LoadingDots interval={ 100 } dots={ 20 }/> } */}
       </nav>
     );
   }
 }
-{/*
-  const Header = ({ loading }) => {
-    return (
-      <nav className="sticky">
-        <IndexLink to="/" activeClassName="active" className="nav-item">HOME</IndexLink>
-
-        <Link to="/skills" activeClassName="active" className="nav-item">SKILLS</Link>
-
-        <Link to="/projects" activeClassName="active" className="nav-item">PROJECTS</Link>
-
-        <Link to="/about" activeClassName="active" className="nav-item">ABOUT ME</Link>
-
-        <Link to="/contact" activeClassName="active" className="nav-item">CONTACT</Link>
-
-        { loading && <LoadingDots interval={ 100 } dots={ 20 }/> }
-      </nav>
-    );
-  };
-*/}
-{/*
-  Header.propTypes = {
-    loading: PropTypes.bool.isRequired
-  };
- */}
 
 export default Header;
--- src/components/skills/SkillsPage.js
+++ src/components/skills/SkillsPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
 class SkillsPage extends React.Component {
   render() {
@@ -8,6 +10,16 @@ class SkillsPage extends React.Component {
         <p>Fulls stack web developer with a background in game design.</p>
 
         Here is a list of all my skills
+        <Chart
+        chartType="ScatterChart"
+        data={[['Age', 'Weight'], [8, 12], [4, 5.5]]}
+        options={{}}
+        graph_id="ScatterChart"
+        width="100%"
+        height="400px"
+        legend_toggle
+       />
+
       </div>
     );
   }
