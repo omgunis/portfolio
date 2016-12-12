a7f728ea3b71ef602af3ff369557989497c319fd
omgunis <eunicegchang@gmail.com>
2016-12-12 10:33:51 -0800

    Remove link to d3

--- src/components/skills/SkillsPage.js
+++ src/components/skills/SkillsPage.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Chart } from 'react-google-charts';
 
 class SkillsPage extends React.Component {
   constructor(props){
     super(props);
-
-    this.state = {
-      options:{
-        title: 'My Daily Activities',
-        pieHole: 0.6,
-        legend: 'none',
-        pieStartAngle: 102,
-        animation:{
-          duration: 1000,
-          easing: 'out',
-        },
-        slices: [{}, {color:'white'}]
-      },
-      data:[
-        ['Task', 'Hours per Day'],
-        ['Work',     10],
-        ['Empty', 4]
-      ]
-    };
   }
   render() {
     return(
@@ -32,15 +12,6 @@ class SkillsPage extends React.Component {
         <p>Fulls stack web developer with a background in game design.</p>
 
         Here is a list of all my skills
-        <Chart
-          chartType="PieChart"
-          data={this.state.data}
-          options={this.state.options}
-          graph_id="donutchart"
-          width="900px"
-          height="500px"
-          legend_toggle
-       />
       </div>
     );
   }
